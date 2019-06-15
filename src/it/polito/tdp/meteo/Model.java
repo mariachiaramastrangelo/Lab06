03e7c521fefa1fe35db76175ef9525b10a41762c
macbook <macbook@mariachmacbook2.station>
2019-06-15 17:53:07 +0200

    esempio algoritmo ricorsivo complesso (non funzionante per l'interfaccia)

--- src/it/polito/tdp/meteo/Model.java
+++ src/it/polito/tdp/meteo/Model.java
@@ -1,9 +1,11 @@
 package it.polito.tdp.meteo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+import it.polito.tdp.meteo.bean.Citta;
 import it.polito.tdp.meteo.bean.SimpleCity;
 import it.polito.tdp.meteo.db.MeteoDAO;
 
@@ -14,21 +16,99 @@ public class Model {
 	private final static int NUMERO_GIORNI_CITTA_MAX = 6;
 	private final static int NUMERO_GIORNI_TOTALI = 15;
 	
-	MeteoDAO md= new MeteoDAO();
+	
+	private List<Citta> leCitta;
 
+	private List<Citta> best;
+	private MeteoDAO dao;
 	public Model() {
+		dao = new MeteoDAO();
+		this.leCitta = dao.getAllCitta();
 
 	}
+	
 
 	public String getUmiditaMedia(int mese) {
 
-		return md.getUmiditaMedia(mese);
+		return dao.getUmiditaMedia(mese);
 	}
 
-	public String trovaSequenza(int mese) {
-
-		return "TODO!";
+	public List<Citta> calcolaSequenza(){
+		List<Citta> parziale= new ArrayList<Citta>();
+		this.best=null;
+		cerca(parziale, 0);
+		return null;
+		
 	}
+	
+	private void cerca(List<Citta> parziale, int livello) {
+		Double costo= calcolaCosto(parziale);
+		//caso terminale
+		if(livello==this.NUMERO_GIORNI_TOTALI) {
+			if (best == null || costo < calcolaCosto(best)) {
+				// System.out.format("%f %s\n", costo, parziale) ;
+
+				best = new ArrayList<>(parziale);
+			}
+		}
+		for(Citta prova: leCitta) {
+			if(aggiuntaValida(prova, parziale)) {
+				parziale.add(prova);
+				
+				cerca(parziale, livello+1);
+				
+				//backtrack
+				parziale.remove(parziale.size()-1);
+			}
+		}
+		
+	}
+	
+	private boolean aggiuntaValida(Citta prova, List<Citta> parziale) {
+		int count=0;
+		//verifica dei giorni massimi 
+		for(Citta precedente: parziale) {
+			if(precedente.equals(prova)) {
+				count++;
+			}
+		}
+		if(count>= this.NUMERO_GIORNI_CITTA_MAX)
+			return false;
+		//verifica giorni minimi
+		//giorno iniziale
+		if(parziale.size()==0) {
+			return true;
+		}
+		//secondo o terzo giorno
+		if(parziale.size()==1 || parziale.size()==2 ) {
+			return parziale.get(parziale.size()-1).equals(prova);
+		}
+		if(parziale.get(parziale.size()-1).equals(prova)) { //giorni successivi e posso rimanere
+			return true;
+		}
+		if(parziale.get(parziale.size()-1).equals(parziale.get(parziale.size()-2)) && parziale.get(parziale.size()-2).equals(parziale.get(parziale.size()-1))) {
+			return true;
+		}
+		return false;
+	}
+
+
+	private Double calcolaCosto(List<Citta> parziale) {
+		double costo= 0.0;
+		for(int i=0; i<=this.NUMERO_GIORNI_TOTALI; i++) {
+			Citta c= parziale.get(i);
+			//che umidità ho nella città in quel giorno
+			double umidita= c.getRilevamenti().get(i).getUmidita();
+			costo+= umidita;
+		}
+		for(int i=2; i<=this.NUMERO_GIORNI_TOTALI; i++) {
+			if(parziale.get(i).equals(parziale.get(i-1))) {
+				costo+=100;
+			}
+		}
+		return costo;
+	}
+
 
 	private Double punteggioSoluzione(List<SimpleCity> soluzioneCandidata) {
 
@@ -42,7 +122,7 @@ public class Model {
 	}
 
 	public HashSet<Integer> getMesi() {
-		return md.getMesi() ;
+		return dao.getMesi() ;
 	}
 
 }
--- src/it/polito/tdp/meteo/db/MeteoDAO.java
+++ src/it/polito/tdp/meteo/db/MeteoDAO.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.sql.Date;
+
+import it.polito.tdp.meteo.bean.Citta;
 import it.polito.tdp.meteo.bean.Rilevamento;
 
 public class MeteoDAO {
@@ -43,6 +45,29 @@ public class MeteoDAO {
 
 	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
 
+		return null;
+	}
+	public List<Rilevamento> getRilevamentiTecnici(int mese){
+		final String sql="SELECT Localita, Data, Umidita " + 
+				"FROM situazione " + 
+				"WHERE DAYOFMONTH(Data)>=1 && DAYOFMONTH(Data)<=15 && MONTH(Data)=? " + 
+				"Order by Localita, Data";
+		List<Rilevamento> rilevamentiTecnici= new ArrayList<Rilevamento>();
+		try {
+			Connection conn= DBConnect.getInstance().getConnection();
+			PreparedStatement st= conn.prepareStatement(sql);
+			st.setInt(1, mese);
+			ResultSet rs= st.executeQuery();
+			
+			while(rs.next()) {
+				rilevamentiTecnici.add(new Rilevamento(rs.getString(1), rs.getDate(2), rs.getInt(3)));
+				
+			}
+			conn.close();
+			
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
 		return null;
 	}
 
@@ -97,6 +122,29 @@ public class MeteoDAO {
 			throw new RuntimeException(e);
 		}
 		return umiditaMedia;
+	}
+
+	public List<Citta> getAllCitta() {
+		String sql = "SELECT DISTINCT localita FROM situazione ORDER BY localita";
+
+		List<Citta> result = new ArrayList<>();
+
+		try {
+			Connection conn = DBConnect.getInstance().getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				result.add(new Citta(res.getString("localita")));
+			}
+
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
 	}
 
 }
